5cc7b087d45a5e373df062d02e55f8403ad33bc6
annie <59670172+annie-dahye-lee@users.noreply.github.com>
2021-12-06 10:02:41 -0800

    Add files via upload

--- Question1.java
+++ Question1.java
@@ -1,10 +1,46 @@
+import java.util.*;
+import java.util.ArrayList;
 
-public class Question1
-{
-    
-    
+public class Question1{
+    //private static 
+    public static ArrayList<Integer> sieveOfEratosthenes(Integer n) {
+        ArrayList<Integer> list = new ArrayList<Integer>(20);
+        Integer p = new Integer(2);
+      
+        
+        /*
+        // remove every composite number
+        for (int i = 0; i < 8; i++) {
+            if (list.get(i) % p == 0 ) {
+                list.remove(i);
+            }
+        }
+        
+        
+        /*
+        while(p < n) {
+            Integer current = list.get(i);
+            if (current % p == 0 && current > p) {
+                list.remove(i);
+            }
+            
+            if (current > p && current <= n) {
+                p = i;
+            }
+            
+            i++;
+        }
+        
+        */
+        System.out.println("original list: " + list);
+        
+        return list;
+    }
     
     public static void main(String[] args) {
-        
+        System.out.println('\u000C');
+        Integer max = new Integer(20);
+        sieveOfEratosthenes(max);
+        System.out.println("updated list: " + sieveOfEratosthenes(max));
     }
 }
